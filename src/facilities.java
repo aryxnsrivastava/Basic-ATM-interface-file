5a15ca1bc8cd30956a88ef7b59512789e89ecf9a
Aryan Shashikumar Srivastava <146150888+aryxnsrivastava@users.noreply.github.com>
2023-09-26 23:58:01 -0300

    Update facilities.java

--- src/facilities.java
+++ src/facilities.java
@@ -1,14 +1,20 @@
 import java.util.*;
 public class facilities extends Accounts{
+
+    //constructor
     public facilities(int accountNUMBER, int pinCODE, int balance, String name){
         super(accountNUMBER, pinCODE, balance, name);
     }
 
+    
+    //method to deposit money
     public void deposit(int depositAmount){
         this.balance += depositAmount;
         System.out.println("The new balance is: " + this.balance);
     }
 
+
+    //method to withdraw money if there's enough balance left.
     public void withdrawal(int withdrawalAmount){
         if(this.balance >  withdrawalAmount){
             this.balance -= withdrawalAmount;
