3dc46513a2ab8db9b49639397768ed929d4dd69b
Aryan Shashikumar Srivastava <146150888+aryxnsrivastava@users.noreply.github.com>
2023-09-26 23:55:10 -0300

    Update Accounts.java

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 
 public class Accounts{
+
+    //instance variables
     int accountNUMBER = 0;
     int pinCODE = 0;
     int balance = 0;
 
     String name = "";
 
+    //constructor
     public Accounts(int accountNUMBER, int pinCODE, int balance, String name){
         this.accountNUMBER = accountNUMBER;
         this.pinCODE = pinCODE;
@@ -14,9 +17,12 @@ public class Accounts{
         this.name = name;
     }
 
+    //setter
     public void setBalance(int balance){
         this.balance = balance;
     }
+
+    //getter
     public int getBalance(){
         return balance;
     }
